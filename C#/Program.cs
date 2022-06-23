4f8557d0c7183f683a1a1355b84c051215c97eda
Zaki <zakiabdulle1@gmail.com>
2022-06-23 21:54:43 +0100

    done

--- C#/Program.cs
+++ C#/Program.cs
@@ -7,34 +7,40 @@ namespace Company
     {
         static void Main(string[] args)
         {
-            // // override toString()
-            // var testNode = new DerivedNode("hello", 1, 1);
-            // Console.WriteLine();
-
-            // // Euclidean distance
-            // Console.WriteLine(testNode.ToString());
-            // Console.WriteLine();
-            // Console.WriteLine(testNode.GetDistance(10, 10));
-            // Console.WriteLine();
-
-            // // ListA
-            // var list_one = GenerateRandomDerivedNodes(20);
-            // list_one.ForEach(x => Console.WriteLine(x.ToString()));
-            // Console.WriteLine("");
-
-            // // ListB
-            // var list_two = GenerateRandomDerivedNodes(20);
-            // list_two.ForEach(x => Console.WriteLine(x.ToString()));
-            // Console.WriteLine("");
-
-            // // Combined and filtered lists
-            // var filteredAndCombinedNodeList = ProcessNodes(list_one, list_two);
-            // filteredAndCombinedNodeList.ForEach(x => Console.WriteLine(x.ToString()));
+            // Start async process
             var testingAsync = LongRunningProcess();
-           
-            Console.WriteLine(testingAsync);
 
+            // override toString()
+            var testNode = new DerivedNode("hello", 1, 1);
+            Console.WriteLine();
 
+            // Euclidean distance
+            Console.WriteLine(testNode.ToString());
+            Console.WriteLine();
+            Console.WriteLine(testNode.GetDistance(10, 10));
+            Console.WriteLine();
+
+            // ListA
+            var list_one = GenerateRandomDerivedNodes(20);
+            list_one.ForEach(x => Console.WriteLine(x.ToString()));
+            Console.WriteLine();
+
+            // ListB
+            var list_two = GenerateRandomDerivedNodes(20);
+            list_two.ForEach(x => Console.WriteLine(x.ToString()));
+            Console.WriteLine();
+
+            // Combined and filtered lists
+            var filteredAndCombinedNodeList = ProcessNodes(list_one, list_two);
+            filteredAndCombinedNodeList.ForEach(x => Console.WriteLine(x.ToString()));
+            Console.WriteLine();
+
+            // wait for and return async task
+            testingAsync.Wait();
+            var done = testingAsync.Result;
+            Console.WriteLine(done[0]);
+            Console.WriteLine(done[1]);
+            Console.WriteLine(done[2]);
         }
 
     }
